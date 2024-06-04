2f659e31c79f1da01226b626c92e94c6d096660e
Muny1re1 <munyiriandy@gmail.com>
2024-06-04 13:05:33 +0300

    labDone

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1,"Mr. Chocolate", 20, "F", "dark brown", "calm", 0);
+INSERT INTO bears VALUES (2,"Rowdy", 10, "F", "black", "intense", 1);
+INSERT INTO bears VALUES (3,"Tabitha", 6, "M", "dark brown", "Nice", 1);
+INSERT INTO bears VALUES (4,"Sergeant Brown", 19, "F", "Green", "Slimy", 1);
+INSERT INTO bears VALUES (5,"Melissa", 13, "M", "dark brown", "goofy", 0);
+INSERT INTO bears VALUES (6,"Grinch", 2, "M", "Black", "Grinchy", 1);
+INSERT INTO bears VALUES (7,"Wendy", 12, "F", "Blue", "naive", 1);
+INSERT INTO bears VALUES (8,null, 20, "M", "black", "aggressive", 0);
